b16fc7c9104c833534c209e257e18b9e27b3b280
Duy Nguyen <duynguyen@Duys-MacBook-Pro-2.local>
2023-10-24 16:00:54 +0700

    initial

--- src/router/routes/routes.public.module.ts
+++ src/router/routes/routes.public.module.ts
@@ -6,31 +6,13 @@ import { MessagePublicController } from 'src/core/message/controllers/message.pu
 import { CqrsModule } from '@nestjs/cqrs';
 import { UserPublicController } from 'src/modules/user/controllers/user.public.controller';
 import { UserModule } from 'src/modules/user/user.module';
-import { ParkingTicketModule } from 'src/modules/parking-ticket/parking-ticket.module';
-import { ParkingTicketPublicController } from 'src/modules/parking-ticket/controllers/parking-ticket.public.controller';
-import { ParkingSpotPublicController } from 'src/modules/parking-spot/controllers/parking-spot.public.controller';
-import { ParkingSpotModule } from 'src/modules/parking-spot/parking-spot.module';
-import { PaymentPublicController } from 'src/modules/payment/controllers/payment.public.controller';
-import { PaymentModule } from 'src/modules/payment/payment.module';
 
 @Module({
-    imports: [
-        CqrsModule,
-        UserModule,
-        TerminusModule,
-        HealthModule,
-        UserModule,
-        ParkingTicketModule,
-        ParkingSpotModule,
-        PaymentModule,
-    ],
+    imports: [CqrsModule, UserModule, TerminusModule, HealthModule, UserModule],
     controllers: [
         HealthPublicController,
         MessagePublicController,
         UserPublicController,
-        ParkingTicketPublicController,
-        ParkingSpotPublicController,
-        PaymentPublicController,
     ],
     providers: [],
     exports: [],
